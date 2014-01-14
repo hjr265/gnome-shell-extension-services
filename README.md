84d1a1ecb841e8f6fbd9370d15f0498c74c4dcd8
Mahmud Ridwan <m@hjr265.me>
2014-01-15 01:57:30 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Services (Gnome Shell Extension)
+
+A Gnome Shell Extension that allows you to toggle some common services on/off from a menu on the top panel.
+
+## Services
+
+- MongoDB
+
+- MySQL
+
+- PostgreSQL
+
+- Redis
+
+## Installation
+
+    $ git clone git://github.com/hjr265/gnome-shell-extension-services.git
+    $ cd gnome-shell-extension-services
+    # cp -r services@hjr265.me ~/.local/share/gnome-shell/extensions
+
+Restart Gnome Shell and enable the extension
+
+## License
+
+This extension is available under the [GNU General Public License, version 2](http://www.gnu.org/licenses/gpl-2.0.html).
---
+++ services@hjr265.me/extension.js
@@ -0,0 +1,125 @@
+const GLib = imports.gi.GLib;
+const Lang = imports.lang;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const St = imports.gi.St;
+
+
+let SERVICES = [
+	{
+		name: 'mongodb',
+		label: 'MongoDB',
+		probes: [
+			{
+				type: 'systemd',
+				unit: 'mongodb.service'
+			}
+		]
+	},
+	{
+		name: 'mysql',
+		label: 'MySQL',
+		probes: [
+			{
+				type: 'systemd',
+				unit: 'mysqld.service'
+			}
+		]
+	},
+	{
+		name: 'postgresql',
+		label: 'PostgreSQL',
+		probes: [
+			{
+				type: 'systemd',
+				unit: 'postgresql.service'
+			}
+		]
+	},
+	{
+		name: 'redis',
+		label: 'Redis',
+		probes: [
+			{
+				type: 'systemd',
+				unit: 'redis.service'
+			}
+		]
+	}
+];
+
+
+function Services() {
+	this.button = new PanelMenu.Button(0.0);
+	var actor = new St.Icon({
+		icon_name: 'system-run-symbolic',
+		style_class: 'system-status-icon'
+	});
+	this.button.actor.add_actor(actor);
+	this.button.actor.add_style_class_name('panel-status-button');
+
+	this.button.actor.connect('button-press-event', Lang.bind(this, function() {
+		this.refresh();
+	}));
+	this.refresh();
+
+	Main.panel.addToStatusArea('services', this.button);
+}
+Services.prototype = {
+	refresh: function() {
+		this.button.menu.removeAll();
+		SERVICES.forEach(Lang.bind(this, function(service) {
+			let found = false;
+			let active = false;
+			let probe = service.probes.filter(function(probe) {
+				if(found) {
+					return false;
+				}
+				switch(probe.type) {
+					case 'systemd':
+						let [_, out] = GLib.spawn_command_line_sync('systemctl status --no-block ' + probe.unit);
+						out = out.toString();
+						if(out.match(/Loaded: loaded/g)) {
+							found = true;
+							if(out.match(/Active: (active|activating)/g)) {
+								active = true;
+							}
+							return true;
+						}
+						break;
+				}
+			})[0];
+			if(found) {
+				let item = new PopupMenu.PopupSwitchMenuItem(service.label, active);
+				this.button.menu.addMenuItem(item);
+				item.connect('toggled', function() {
+					switch(probe.type) {
+						case 'systemd':
+							GLib.spawn_command_line_async('sh -c "pkexec --user root systemctl ' + (active ? 'stop' : 'start') + ' ' + probe.unit + '; sleep 1; exit;"');
+							break;
+					}
+				});
+			}
+		}));
+	},
+
+	destroy: function() {
+		this.button.destroy();
+	}
+};
+
+
+function init() {
+
+}
+
+var services;
+
+function enable() {
+	services = new Services();
+}
+
+function disable() {
+	services.destroy();
+}
